b9ea446cd36afe62fd8640f8a8e86a12bd2e49ce
Thiago Caiado <thiago.caiado@hotmail.com>
2026-01-17 23:32:22 -0300

    app colors implementation

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/application/resources/color_scheme.dart';
 import 'package:tic_tac_toe/application/routes/routes.dart';
+
+import 'package:tic_tac_toe/application/resources/theme/app_colors.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,7 +15,8 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        colorScheme: colorScheme,
       ),
       initialRoute: Routes.home,
       routes: Routes.routes,
---
+++ lib/application/resources/color_scheme.dart
@@ -0,0 +1,25 @@
+// ignore: non_constant_identifier_names
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/application/resources/theme/app_colors.dart';
+
+const colorScheme =  ColorScheme(
+  brightness: Brightness.light,
+  primary: AppColors.primary,
+  onPrimary: AppColors.onPrimary,
+  primaryContainer: AppColors.primaryContainer,
+  onPrimaryContainer: AppColors.onPrimaryContainer,
+  secondary: AppColors.secondary,
+  onSecondary: AppColors.onSecondary,
+  secondaryContainer: AppColors.secondaryContainer,
+  onSecondaryContainer: AppColors.onSecondaryContainer,
+  tertiary: AppColors.tertiary,
+  onTertiary: AppColors.onTertiary,
+  tertiaryContainer: AppColors.tertiaryContainer,
+  onTertiaryContainer: AppColors.onTertiaryContainer,
+  error: AppColors.error,
+  onError: AppColors.onError,
+  errorContainer: AppColors.errorContainer,
+  onErrorContainer: AppColors.onErrorContainer,
+  surface: AppColors.surface,
+  onSurface: AppColors.onSurface,
+);
