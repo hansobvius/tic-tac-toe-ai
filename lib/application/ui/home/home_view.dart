29a03c47c9e3da8ffac8544bcd7e0c3a088f3e06
Thiago Caiado <thiago.caiado@hotmail.com>
2026-01-17 23:55:47 -0300

    add a home preview to check out the main view

--- lib/application/ui/home/home_view.dart
+++ lib/application/ui/home/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widget_previews.dart';
+
 import 'package:tic_tac_toe/application/dependency_injection/dependency.dart';
+import 'package:tic_tac_toe/application/resources/theme/app_colors.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({super.key});
@@ -10,27 +13,46 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: AppColors.onBackground,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.onPrimary,
         title: const Text('TIC TAC TOE'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Welcome to Tic Tac Toe!'),
-            Text(
-              'Start playing now',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('Welcome to Tic Tac Toe!', style: Theme.of(context).textTheme.headlineLarge),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: 3,
+              itemBuilder: (context, rowIndex) {
+                return ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: false,
+                  itemCount: 3,
+                  itemBuilder: (context, columnIndex) {
+                    return Container(
+                      color: Colors.amber,
+                      padding: EdgeInsets.all(.16),
+                      child: Text("$rowIndex / $columnIndex"),
+                    );
+                });
+            })
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'New Game',
-        child: const Icon(Icons.play_arrow),
-      ),
     );
   }
+}
+
+@Preview()
+Widget homePreview() {
+  DependencyInjection().initialize();
+  return MaterialApp(
+    home: HomeView(),
+  );
 }
